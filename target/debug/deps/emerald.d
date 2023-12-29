5fc09890493c73a3e9d4a9e9a520d985f83263d3
Edward-789 <142745633+Edward-789@users.noreply.github.com>
2023-12-29 21:34:06 +0000

    oops
    accidentally added target dir, will add gitignore soon

--- target/debug/deps/emerald.d
+++
@@ -1,7 +0,0 @@
-C:\Users\bilbo\Documents\My projects\Emerald\target\debug\deps\emerald.exe: src\main.rs src\board.rs src\consts.rs
-
-C:\Users\bilbo\Documents\My projects\Emerald\target\debug\deps\emerald.d: src\main.rs src\board.rs src\consts.rs
-
-src\main.rs:
-src\board.rs:
-src\consts.rs:
