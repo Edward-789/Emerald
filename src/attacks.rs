0b8eac88dfdd04d1c937ac5e0f4f772396ab6921
Edward-789 <eplas315@gmail.com>
2024-01-03 21:38:37 +0000

    add a lot of stuff
    -utils.rs (bitshift stuff)
    -refactor pawn moves using these bitshifts
    -add knight attack fn

--- src/attacks.rs
+++ src/attacks.rs
@@ -1,44 +1,55 @@
-use crate::consts::{
-    Colour,
+use crate::{
+    consts::Colour,
+    utils::{
+        shifts
+    }
 };
 
 static mut PAWN_TABLES: [u64; 128] = [0; 128];
+static mut KNIGHT_TABLES: [u64; 128] = [0; 128];
 
 pub struct Attacks;
 impl Attacks {
    pub fn init_tables() {
         unsafe {
-            //pawn tables
             for i in 0..64 {
+                //pawn tables
                 PAWN_TABLES[i] = Self::slow_pawn_attacks(i, 0);
                 PAWN_TABLES[i + 64] = Self::slow_pawn_attacks(i, 1);
+
+                //knight tables
+                KNIGHT_TABLES[i] = Self::slow_knight_attacks(i);
             }
         }
     }
+
     pub fn pawn_attacks(square : usize, colour_num : usize) -> u64 {
         unsafe { PAWN_TABLES[square + (64 * colour_num)] }
     }
+
+    pub fn knight_attacks(square : usize) -> u64 {
+        unsafe { KNIGHT_TABLES[square] }
+    }
+
     fn slow_pawn_attacks(square : usize, colour_num : usize) -> u64 {
         assert!(colour_num == 1 || colour_num == 0);
 
-        let colour = if colour_num == 0 {Colour::White} else {Colour::Black};
+        let pawn = 1 << square;
+        let pawn_push = if colour_num == 0 { shifts::shift_north(pawn) } else { shifts::shift_south(pawn) };
 
-        let square_diagonal_left = square as i16 + if colour == Colour::White {7} else {-9} as i16;
-        let square_diagonal_right = square as i16 + if colour == Colour::White {9} else {-7} as i16;
-
-        if colour == Colour::Black && square / 8 == 0 || colour == Colour::White && square / 8 == 7{
-            return 0u64
-        }
-
-        let file = square % 8;
-
-        match file {
-            0 => 1u64 << square_diagonal_right,
-            7 => 1u64 << square_diagonal_left,
-            _ => (1u64 << square_diagonal_left) | (1u64 << square_diagonal_right),
-        }
-
+        shifts::shift_west(pawn_push) | shifts::shift_east(pawn_push)
     }
 
-    
+    fn slow_knight_attacks(square : usize) -> u64 {
+        let knight = 1 << square;
+
+        shifts::shift_north(shifts::shift_north(shifts::shift_east(knight))) |
+        shifts::shift_north(shifts::shift_north(shifts::shift_west(knight))) |
+        shifts::shift_east(shifts::shift_east(shifts::shift_north(knight))) |
+        shifts::shift_east(shifts::shift_east(shifts::shift_south(knight))) |
+        shifts::shift_south(shifts::shift_south(shifts::shift_east(knight))) |
+        shifts::shift_south(shifts::shift_south(shifts::shift_west(knight))) |
+        shifts::shift_west(shifts::shift_west(shifts::shift_north(knight))) |
+        shifts::shift_west(shifts::shift_west(shifts::shift_south(knight)))
+    }
 }   
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod board;
 mod consts;
 mod attacks;
+mod utils;
 
 use crate::{
     board::Board,
@@ -11,9 +12,9 @@ fn main() {
     Attacks::init_tables();
     println!("Attacks");
 
-    Board::print_bitboard(Attacks::pawn_attacks(Board::find_square(1, 7), 0));
+    Board::print_bitboard(Attacks::knight_attacks(Board::find_square(1, 7)));
     println!();
-    Board::print_bitboard(Attacks::pawn_attacks(Board::find_square(3, 3), 0));
+    Board::print_bitboard(Attacks::knight_attacks(Board::find_square(3, 3)));
     println!();
-    Board::print_bitboard(Attacks::pawn_attacks(Board::find_square(3, 3), 1));
+    Board::print_bitboard(Attacks::knight_attacks(Board::find_square(3, 3)));
 } 
