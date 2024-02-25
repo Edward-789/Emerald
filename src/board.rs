4017ea430343f4649e0ffb538801ed9e99eea64c
Edward-789 <eplas315@gmail.com>
2024-02-25 14:48:57 +0000

    add load position stuff
    able to load positions in form of position fen x moves y

--- src/board.rs
+++ src/board.rs
@@ -19,6 +19,8 @@ pub struct Board {
 impl Board {
 
     // utilities
+    pub const STARTPOS: &'static str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
     fn square_is_occupied(&self, square: usize) -> bool {
         ((self.bitboards[0] | self.bitboards[1]) & (1 << square)) > 0
     }
@@ -333,6 +335,42 @@ impl Board {
         !self.square_is_attacked(self.king_squares[orig_colour as usize] as usize, orig_colour, self.bitboards[0] | self.bitboards[1])
     }
 
+    pub fn apply_uci_move(&mut self, mov: &str){  // get first and last 2 characters of move
+        let from_square = Self::square_from_str(&mov[0..2]);
+        let to_square = Self::square_from_str(&mov[2..4]);
+
+        let mut flag = Move::NO_FLAG;
+        let moving_piece = self.piece_type(from_square).unwrap();
+
+        if moving_piece == Pieces::King {
+            flag = match mov {
+                "e1g1" => Move::WHITE_KINGSIDE,
+                "e1c1" => Move::WHITE_QUEENSIDE,
+                "e8g8" => Move::BLACK_KINGSIDE,
+                "e8c8" => Move::BLACK_QUEENSIDE,
+                _ => Move::NO_FLAG
+            }
+        } else if moving_piece == Pieces::Pawn {
+            if mov.len() == 5 {
+                flag = match &mov.chars().nth(4).unwrap() {
+                    'q' => Move::QUEEEN_PROMO,
+                    'r' => Move::ROOK_PROMO,
+                    'n' => Move::KNIGHT_PROMO,
+                    'b' => Move::BISHOP_PROMO,
+                    _  => Move::NO_FLAG
+                }
+            } else {
+                let neccesary_shift = to_square.abs_diff(from_square);
+                if neccesary_shift == 16 {
+                    flag = Move::DOUBLE_PAWN_PUSH
+                } else if neccesary_shift != 8 && !self.square_is_occupied(to_square) {
+                    flag = Move::EN_PASSANT
+                }
+            }
+        }
+
+        assert!(self.apply(Move::new(from_square, to_square, flag)));
+    }
      // debug stuff
      #[allow(dead_code)]
 
--- src/uci.rs
+++ src/uci.rs
@@ -1,22 +1,66 @@
-use std::io;
+use std::{io, time::Instant};
 
-use crate::perft::run_perft_suite;
+use crate::{
+    board::Board, 
+    perft::{perft, run_perft_suite}
+};
 
 pub fn uci_loop() {
+    let mut board = Board::read_fen(Board::STARTPOS);
     loop {
         let mut input = String::new();
 
         io::stdin().read_line(&mut input).unwrap();
 
-        handle_command(input.trim());
+        let command = input.trim();
+        let split_command = command.split(' ').collect::<Vec<&str>>();
+
+        if command == "quit" {
+            break;
+        } else if command == "perftsuite" {
+            run_perft_suite();
+        } else if split_command[0] == "perft" && split_command[1].parse::<u8>().is_ok() {
+            let start_time = Instant::now();
+            let nodes = perft::<false, false>(&board, split_command[1].parse::<u8>().unwrap());
+
+            println!("{}{}", "Nodes : ", nodes);
+            println!("{}{}", "Milliseconds : ", start_time.elapsed().as_millis());
+            println!("{}{}", "NPS : ", (nodes as u128 / (start_time.elapsed().as_millis() + 1)) * 1000);
+        } else if split_command[0] == "splitperft" && split_command[1].parse::<u8>().is_ok() {
+            let start_time = Instant::now();
+            let nodes = perft::<false, false>(&board, split_command[1].parse::<u8>().unwrap());
+
+            println!();
+            println!("{}{}", "Milliseconds : ", start_time.elapsed().as_millis());
+            println!("{}{}", "NPS : ", (nodes as u128 / (start_time.elapsed().as_millis() + 1)) * 1000);
+        } else if split_command[0] == "position" {
+            board = load_position(split_command)
+        }
     }
 }
 
-fn handle_command(command: &str) {
-    match command {
-        "uci" => println!("uciok"),
-        "ready" => println!("readyok"),
-        "perftsuite" => run_perft_suite(),
-        _ => {}
+fn load_position(split_command : Vec<&str>) -> Board {
+    let mut board = Board::read_fen(Board::STARTPOS);
+
+    if split_command[1] == "fen" {
+        let mut fen = String::new();
+        let mut move_start_index = 0;
+        for i in 2..split_command.len() {
+            move_start_index = i;
+            if split_command[i] == "moves" {
+                break;
+            };
+            fen += split_command[i];
+            fen += " ";
+        }
+
+        fen = fen.trim().to_string();   // trim trailing whitespace
+        board = Board::read_fen(&fen);
+
+        for i in (move_start_index + 1)..split_command.len() {
+            board.apply_uci_move(split_command[i]);
+        }
     }
+
+    board
 }
